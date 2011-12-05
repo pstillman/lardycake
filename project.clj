fe2b0202f53f9b0bacab44ea43c6355b7cc62396
Peter Stillman <peter.stillman@gmail.com>
2011-12-05 16:53:54 -0500

    Add clj-jgit to the mix.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject lardycake "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+            :description "A noir app for fiddling with SCMs"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [clj-jgit "0.0.2"]]
+            :dev-dependencies [[lein-ring "0.4.6"]
+                               [org.clojure/clojure-contrib "1.2.0"]]
             :main lardycake.server)
 
